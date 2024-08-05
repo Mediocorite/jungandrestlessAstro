b68dbba1191094e10f03f1d1c7393f7c2e0dd881
Anzar Ahmad Malik <12586091+Mediocorite@users.noreply.github.com>
2024-08-05 01:19:01 +0100

    Stage one: Completed the tutorial

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
-
 import netlify from "@astrojs/netlify";
+
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://www.jungandrestless.com",
   output: "server",
-  adapter: netlify()
+  adapter: netlify(),
+  integrations: [preact()]
 });
