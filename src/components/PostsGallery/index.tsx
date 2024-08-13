d014b26f5f44614d5107ea8aa0e7bda4096f9e98
Anzar Ahmad Malik <12586091+Mediocorite@users.noreply.github.com>
2024-08-13 22:38:33 +0100

    Basic component CSS

--- src/components/PostsGallery/index.tsx
+++ src/components/PostsGallery/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default function PostsGallery(props) {
+export default function PostsGallery() {
     return (
         <div className="post-gallery">
             posts
